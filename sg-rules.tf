c8661f59db5786efc796977326ca095415f9d63f
Naveen <hi.to.cloud@gmail.com>
2024-10-26 10:55:49 +0530

    Added Expense-TF-SG

---
+++ sg-rules.tf
@@ -0,0 +1,103 @@
+### DB
+resource "aws_security_group_rule" "db-be" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = module.sg-made-easy-be.sg_id
+  security_group_id        = module.sg-made-easy-db.sg_id
+}
+
+resource "aws_security_group_rule" "db-bastion" {
+  type                     = "ingress"
+  from_port                = 3306
+  to_port                  = 3306
+  protocol                 = "tcp"
+  source_security_group_id = module.sg-made-easy-bastion.sg_id
+  security_group_id        = module.sg-made-easy-db.sg_id
+}
+
+### BE
+resource "aws_security_group_rule" "be-fe" {
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  source_security_group_id = module.sg-made-easy-fe.sg_id
+  security_group_id        = module.sg-made-easy-be.sg_id
+}
+
+resource "aws_security_group_rule" "be-bastion" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = module.sg-made-easy-bastion.sg_id
+  security_group_id        = module.sg-made-easy-be.sg_id
+}
+
+resource "aws_security_group_rule" "be-ansible" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = module.sg-made-easy-ansible.sg_id
+  security_group_id        = module.sg-made-easy-be.sg_id
+}
+
+### FE
+resource "aws_security_group_rule" "fe-public" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.sg-made-easy-fe.sg_id
+}
+
+resource "aws_security_group_rule" "fe-be" {
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  source_security_group_id = module.sg-made-easy-be.sg_id
+  security_group_id        = module.sg-made-easy-fe.sg_id
+}
+
+resource "aws_security_group_rule" "fe-bastion" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = module.sg-made-easy-bastion.sg_id
+  security_group_id        = module.sg-made-easy-fe.sg_id
+}
+
+resource "aws_security_group_rule" "fe-ansible" {
+  type                     = "ingress"
+  from_port                = 22
+  to_port                  = 22
+  protocol                 = "tcp"
+  source_security_group_id = module.sg-made-easy-ansible.sg_id
+  security_group_id        = module.sg-made-easy-fe.sg_id
+}
+
+### Bastion
+resource "aws_security_group_rule" "bastion_public" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.sg-made-easy-bastion.sg_id
+}
+
+### Ansible
+resource "aws_security_group_rule" "ansible_public" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = module.sg-made-easy-ansible.sg_id
+}
---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "project_name" {
+  type    = string
+  default = "expense"
+}
+
+variable "environment" {
+  type    = string
+  default = "dev"
+}
+
+variable "common_tags" {
+  type = map(any)
+  default = {
+    Project     = "expense",
+    Environment = "dev",
+    Terraform   = "true"
+  }
+}
+
+variable "egress" {
+  type = list(any)
+  default = [{
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }]
+}
+
+variable "ingress" {
+  type    = list(any)
+  default = []
+}
